266c11cf3f291e11ec291e8865e03d180c7b88aa
shuhag <shuhag200113@gmail.com>
2025-05-17 15:32:35 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SellerAuthComponent } from './pages/seller-auth/seller-auth.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'seller-auth',component:SellerAuthComponent},
+    {path:"**",component:NotFoundComponent}
+];
